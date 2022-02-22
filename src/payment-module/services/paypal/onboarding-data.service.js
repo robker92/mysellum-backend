d871b8ecd842b17a04f35c40d5bdecafa7900130
Robert <robert.kerscher@sap.com>
2022-02-22 08:19:05 +0100

    updates

--- src/payment-module/services/paypal/onboarding-data.service.js
+++ src/payment-module/services/paypal/onboarding-data.service.js
@@ -9,8 +9,11 @@ import {
     databaseEntity,
 } from '../../../storage/database-operations';
 import { PAYPAL_PLATFORM_MERCHANT_ID } from '../../../config';
-import { fetchAndValidateStore } from '../../utils/order-utils';
 import { paypalClient, getAccessToken } from './client/rest/paypal-rest-client';
+import { storeActivationRoutine } from '../../../store-module/services/activation.service';
+
+// MongoDB transaction
+import { getMongoDbClient, getMongoDbTransactionWriteOptions } from '../../../storage/mongodb/setup';
 
 export { onboardingDataService };
 
@@ -26,50 +29,104 @@ async function onboardingDataService(
     isEmailConfirmed,
     accountStatus
 ) {
+    const session = getMongoDbClient().startSession();
     try {
-        console.log('hi');
-        // Validate Paypal Ids
-        await validatePaypalMerchantId(merchantIdInPayPal);
-        // Validate store id & store owner & check if values are not already set
-        // save data to store (paypal + status)
+        await session.withTransaction(async () => {
+            // Validate Paypal Ids
+            await validatePaypalMerchantId(merchantIdInPayPal);
+            // Validate store id & store owner & check if values are not already set
+            // save data to store (paypal + status)
 
-        // TODO check activation
-        console.log(`Received store id: ${storeId}`);
-        // Validate store id & check if values are not already set
-        const store = await fetchAndValidateStore(storeId);
-        if (store.payment.paypal.common.merchantIdInPayPal) {
-            throw new Error('There is already a merchantIdInPayPal stored for this store.');
-        }
-        if (store.userEmail !== userEmail) {
-            throw new Error('User not authorized to edit this store.');
-        }
+            console.log(`Received store id: ${storeId}`);
 
-        // save data to store (paypal + status)
-        // TODO check status
-        await updateOneOperation(
-            databaseEntity.STORES,
-            {
-                _id: storeId,
-            },
-            {
-                'payment.registered': true,
-                'payment.paypal.common.merchantId': merchantId,
-                'payment.paypal.common.merchantIdInPayPal': merchantIdInPayPal,
-                'payment.paypal.common.permissionsGranted': permissionsGranted,
-                'payment.paypal.common.consentStatus': consentStatus,
-                'payment.paypal.common.productIntentId': productIntentId,
-                'payment.paypal.common.productIntentID': productIntentID,
-                'payment.paypal.common.consentStatus': consentStatus,
-                'payment.paypal.common.isEmailConfirmed': isEmailConfirmed,
-                'payment.paypal.common.accountStatus': accountStatus,
-                'activationSteps.paymentMethodRegistered': true,
-            },
-            'set'
-        );
+            // Validate store id & check if values are not already set
+            const store = await fetchAndValidateStoreForOnboarding(storeId, session);
+
+            // User should be able to change connected paypal accounts -> check disabled
+            // if (store.payment.paypal.common.merchantIdInPayPal) {
+            //     throw new Error('There is already a merchantIdInPayPal stored for this store.');
+            // }
+            if (store.userEmail !== userEmail) {
+                throw new Error('User not authorized to edit this store.');
+            }
+
+            // save data to store (paypal + status)
+            await updateOneOperation(
+                databaseEntity.STORES,
+                {
+                    _id: storeId,
+                },
+                {
+                    'payment.registered': true,
+                    'payment.paypal.common.merchantId': merchantId,
+                    'payment.paypal.common.merchantIdInPayPal': merchantIdInPayPal,
+                    'payment.paypal.common.permissionsGranted': permissionsGranted,
+                    'payment.paypal.common.consentStatus': consentStatus,
+                    'payment.paypal.common.productIntentId': productIntentId,
+                    'payment.paypal.common.productIntentID': productIntentID,
+                    'payment.paypal.common.consentStatus': consentStatus,
+                    'payment.paypal.common.isEmailConfirmed': isEmailConfirmed,
+                    'payment.paypal.common.accountStatus': accountStatus,
+                    'activationSteps.paymentMethodRegistered': true,
+                },
+                'set',
+                session
+            );
+
+            await storeActivationRoutine(store, session);
+        }, getMongoDbTransactionWriteOptions());
     } catch (error) {
+        console.log('The transaction was aborted due to an unexpected error: ');
         console.log(error);
         throw error;
+    } finally {
+        await session.endSession();
     }
+
+    // try {
+    //     // Validate Paypal Ids
+    //     await validatePaypalMerchantId(merchantIdInPayPal);
+    //     // Validate store id & store owner & check if values are not already set
+    //     // save data to store (paypal + status)
+
+    //     // TODO check activation
+    //     console.log(`Received store id: ${storeId}`);
+    //     // Validate store id & check if values are not already set
+    //     const store = await fetchAndValidateStoreForOnboarding(storeId);
+    //     if (store.payment.paypal.common.merchantIdInPayPal) {
+    //         throw new Error('There is already a merchantIdInPayPal stored for this store.');
+    //     }
+    //     if (store.userEmail !== userEmail) {
+    //         throw new Error('User not authorized to edit this store.');
+    //     }
+
+    //     // save data to store (paypal + status)
+    //     await updateOneOperation(
+    //         databaseEntity.STORES,
+    //         {
+    //             _id: storeId,
+    //         },
+    //         {
+    //             'payment.registered': true,
+    //             'payment.paypal.common.merchantId': merchantId,
+    //             'payment.paypal.common.merchantIdInPayPal': merchantIdInPayPal,
+    //             'payment.paypal.common.permissionsGranted': permissionsGranted,
+    //             'payment.paypal.common.consentStatus': consentStatus,
+    //             'payment.paypal.common.productIntentId': productIntentId,
+    //             'payment.paypal.common.productIntentID': productIntentID,
+    //             'payment.paypal.common.consentStatus': consentStatus,
+    //             'payment.paypal.common.isEmailConfirmed': isEmailConfirmed,
+    //             'payment.paypal.common.accountStatus': accountStatus,
+    //             'activationSteps.paymentMethodRegistered': true,
+    //         },
+    //         'set'
+    //     );
+
+    //     await storeActivationRoutine(store, session);
+    // } catch (error) {
+    //     console.log(error);
+    //     throw error;
+    // }
 
     return;
 }
@@ -113,4 +170,30 @@ async function validatePaypalMerchantId(merchantIdInPaypal, trackingId) {
     }
 
     return;
+}
+
+/**
+ * The function fetches a store and returns it. When the id is invalid, an error is thrown
+ * @param {string} storeId
+ * @param {string} session mongo db session if needed
+ */
+async function fetchAndValidateStoreForOnboarding(storeId, session = null) {
+    const findResult = await readOneOperation(
+        databaseEntity.STORES,
+        {
+            _id: storeId,
+        },
+        {},
+        session
+    );
+
+    if (!findResult) {
+        throw new ValidationError(`A store with the store id ${storeId} could not be found.`);
+    }
+
+    if (findResult.deleted) {
+        throw new ValidationError(`The store with the store id ${storeId} has been deleted.`);
+    }
+
+    return findResult;
 }
