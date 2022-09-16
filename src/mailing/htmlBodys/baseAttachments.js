adab83ab176c4278dd46b05b5670872586c9c74e
Robert <robert.kerscher@sap.com>
2022-09-16 09:19:27 +0200

    up

--- src/mailing/htmlBodys/baseAttachments.js
+++ src/mailing/htmlBodys/baseAttachments.js
@@ -1,30 +1,32 @@
+import * as path from 'path';
+
 export { mysellumAtt, facebookAtt, instagramAtt, linkedinAtt, twitterAtt };
 
 const mysellumAtt = {
     filename: 'mysellum_with_text.png',
-    path: __dirname + '../../../assets/mailing/images/mysellum_with_text.png',
+    path: path.resolve(__dirname, '../../../assets/mailing/images/mysellum_with_text.png'),
     cid: 'mysellum_logo',
 };
 const facebookAtt = {
     filename: 'facebook2x.png',
-    path: __dirname + '../../../assets/mailing/images/facebook2x.png',
+    path: path.resolve(__dirname, '../../../assets/mailing/images/facebook2x.png'),
     cid: 'facebook_logo',
 };
 
 const instagramAtt = {
     filename: 'instagram2x.png',
-    path: __dirname + '../../../assets/mailing/images/instagram2x.png',
+    path: path.resolve(__dirname, '../../../assets/mailing/images/instagram2x.png'),
     cid: 'instagram_logo',
 };
 
 const linkedinAtt = {
     filename: 'linkedin2x.png',
-    path: __dirname + '../../../assets/mailing/images/linkedin2x.png',
+    path: path.resolve(__dirname, '../../../assets/mailing/images/linkedin2x.png'),
     cid: 'linkedin_logo',
 };
 
 const twitterAtt = {
     filename: 'twitter2x.png',
-    path: __dirname + '../../../assets/mailing/images/twitter2x.png',
+    path: path.resolve(__dirname, '../../../assets/mailing/images/twitter2x.png'),
     cid: 'twitter_logo',
 };
